03959f430de5a3f2c6e21562e0effb216b494b41
gusrlLee <gusrl3204@naver.com>
2022-08-07 01:38:18 +0900

    2022/08/07 implementation simple ray tracing ...

--- include/vec3.h
+++ include/vec3.h
@@ -4,49 +4,72 @@
 #include <cmath>
 #include <iostream>
 
+#include "rtweekend.h"
+
 using std::sqrt;
+using std::fabs;
 
 class vec3 {
-    public:
-        vec3() : e{0,0,0} {}
-        vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+  public:
+    vec3() : e{0, 0, 0} {}
+    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
-        double x() const { return e[0]; }
-        double y() const { return e[1]; }
-        double z() const { return e[2]; }
+    double x() const { return e[0]; }
+    double y() const { return e[1]; }
+    double z() const { return e[2]; }
 
-        vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
-        double operator[](int i) const { return e[i]; }
-        double& operator[](int i) { return e[i]; }
+    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+    double operator[](int i) const { return e[i]; }
+    double &operator[](int i) { return e[i]; }
 
-        vec3& operator+=(const vec3 &v) {
-            e[0] += v.e[0];
-            e[1] += v.e[1];
-            e[2] += v.e[2];
-            return *this;
-        }
+    vec3 &operator+=(const vec3 &v)
+    {
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        e[2] += v.e[2];
+        return *this;
+    }
 
-        vec3& operator*=(const double t) {
-            e[0] *= t;
-            e[1] *= t;
-            e[2] *= t;
-            return *this;
-        }
+    vec3 &operator*=(const double t)
+    {
+        e[0] *= t;
+        e[1] *= t;
+        e[2] *= t;
+        return *this;
+    }
 
-        vec3& operator/=(const double t) {
-            return *this *= 1/t;
-        }
+    vec3 &operator/=(const double t)
+    {
+        return *this *= 1 / t;
+    }
 
-        double length() const {
-            return sqrt(length_squared());
-        }
+    double length() const
+    {
+        return sqrt(length_squared());
+    }
 
-        double length_squared() const {
-            return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
-        }
+    double length_squared() const
+    {
+        return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+    }
 
-    public:
-        double e[3];
+    inline static vec3 random()
+    {
+        return vec3(random_double(), random_double(), random_double());
+    }
+
+    inline static vec3 random(double min, double max)
+    {
+        return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
+    }
+
+    bool near_zero() const {
+        const auto s = 1e-8;
+        return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
+    }
+
+  public:
+    double e[3];
 };
 
 using point3 = vec3;   // 3D point
@@ -95,6 +118,33 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
 
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
+}
+
+inline vec3 random_in_unit_sphere() {
+    while (true) {
+        auto p = vec3::random(-1, 1);
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
+}
+
+inline vec3 random_unit_vector() {
+    auto a = random_double(0, 2 * pi);
+    auto z = random_double(-1, 1);
+    auto r = sqrt(1 - z * z);
+    return vec3(r * cos(a), r * sin(a), z);
+}
+
+inline vec3 random_in_hemisphere(const vec3 &normal) {
+    vec3 in_unit_sphere = random_in_unit_sphere();
+    if (dot(in_unit_sphere, normal) > 0.0) 
+        return in_unit_sphere;
+    else 
+        return -in_unit_sphere;
+}
+
+inline vec3 reflect(const vec3& v, const vec3& n) {
+    return v - 2 * dot(v, n) * n;
 }
 
 #endif
