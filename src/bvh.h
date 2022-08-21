466f787bc99f3ccd23fc47f5374f5b72992ebfd2
gusrlLee <gusrl3204@naver.com>
2022-08-21 22:23:04 +0900

    2022/08/21 study stratified Samples

--- src/bvh.h
+++ src/bvh.h
@@ -30,6 +30,28 @@ class bvh_node : public hittable {
     aabb box;
 };
 
+inline bool box_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis) {
+    aabb box_a;
+    aabb box_b;
+
+    if (!a->bounding_box(0,0, box_a) || !b->bounding_box(0,0, box_b))
+        std::cerr << "No bounding box in bvh_node constructor.\n";
+
+    return box_a.min().e[axis] < box_b.min().e[axis];
+}
+
+bool box_x_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+    return box_compare(a, b, 0);
+}
+
+bool box_y_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+    return box_compare(a, b, 1);
+}
+
+bool box_z_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+    return box_compare(a, b, 2);
+}
+
 bvh_node::bvh_node(
         const std::vector<shared_ptr<hittable>>& src_objects,
         size_t start, size_t end, double time0, double time1) {
@@ -44,7 +66,7 @@ bvh_node::bvh_node(
 
     if (object_span == 1) {
         left = right = objects[start];
-    } else if (object_span = 2) {
+    } else if (object_span == 2) {
         if (comparator(objects[start], objects[start+1])) {
             left = objects[start];
             right = objects[start+1];
@@ -85,26 +107,4 @@ bool bvh_node::bounding_box(double time0, double time1, aabb& output_box) const 
     return true;
 }
 
-
-inline bool box_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis) {
-    aabb box_a;
-    aabb box_b;
-
-    if (!a->bounding_box(0,0, box_a) || !b->bounding_box(0,0, box_b))
-        std::cerr << "No bounding box in bvh_node constructor.\n";
-
-    return box_a.min().e[axis] < box_b.min().e[axis];
-}
-
-bool box_x_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
-    return box_compare(a, b, 0);
-}
-
-bool box_y_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
-    return box_compare(a, b, 1);
-}
-
-bool box_z_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
-    return box_compare(a, b, 2);
-}
 #endif
